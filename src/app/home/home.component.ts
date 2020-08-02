60079f0aae09d8e75ca3658acc0c319241b17d91
mt2606 <nguyenmanhtung848@gmail.com>
2020-08-02 10:36:27 +0700

    set up angular

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,8 @@
-import {Component, OnInit} from '@angular/core';
-import {Course} from "../model/course";
-import {Observable} from "rxjs";
-import {map} from "rxjs/operators";
+import { Component, OnInit } from '@angular/core';
+import { Course } from "../model/course";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+import { AngularFirestore } from '@angular/fire/firestore';
 
 
 
@@ -13,14 +14,31 @@ import {map} from "rxjs/operators";
 export class HomeComponent implements OnInit {
 
 
-    constructor() {
+    courses$: Observable<Course[]>;
+
+    beginnerCourse$: Observable<Course[]>;
+    advancedCourser$: Observable<Course[]>;
+    constructor(private coursesService: CoursesService) {
 
     }
 
     ngOnInit() {
+        this.courses$ = this.db.collection('courses').snapshotChanges()
+            .pipe(map(snaps => {
+                return snaps.map(snap => {
+                    return <Course>{
+                        id: snap.payload.doc.id,
+                        ...(snap.payload.doc.data() as any)
+                    };
+                });
+            }));
 
+        this.beginnerCourse$ = this.courses$.pipe(map(courses => courses.filter(course => 
+             course.categories.includes('BEGINNER')
+        )));
 
-
+        this.advancedCourser$ = this.courses$.pipe(map(courses => courses.filter(course => 
+            course.categories.includes('ADVANCED')
+       )));
     }
-
 }
---
+++ src/app/services/courses.service.ts
@@ -0,0 +1,26 @@
+
+
+
+import { Course } from "../model/course";
+import { Injectable } from "@angular/core";
+import { AngularFirestore } from "@angular/fire/firestore";
+import { Observable } from "rxjs";
+
+@Injectable({ providedIn: 'root' })
+export class CoursesService {
+
+    constructor(private db: AngularFirestore) {
+    }
+
+    loadAllCourses(): Observable<Course[]> {
+        return this.db.collection('courses').snapshotChanges()
+            .pipe(map(snaps => {
+                return snaps.map(snap => {
+                    return <Course>{
+                        id: snap.payload.doc.id,
+                        ...(snap.payload.doc.data() as any)
+                    };
+                });
+            }));
+    }
+}
