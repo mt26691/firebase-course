d57f79488f63602d59e2e729879a59cd0b4921fb
mt2606 <nguyenmanhtung848@gmail.com>
2020-08-02 15:59:39 +0700

    batch write

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'about',
@@ -7,9 +9,34 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private db: AngularFirestore) { }
 
   ngOnInit() {
+
+  }
+
+  save() {
+    const firebaseCourseRef = this.db.doc('/courses/485Nc6rcz0x4CxgW62KT').ref;
+    const firebaseCourseRef1 = this.db.doc('/courses/7t73EL7of191Xn9KqCbv').ref;
+
+    const batch = this.db.firestore.batch();
+
+    batch.update(firebaseCourseRef1, {
+      titles: {
+        description: 'Serverless Angular with Firebase Course1',
+        longDescription: 'Serveless Angular with Firestore, Firebase Storage & Hosting, Firebase Cloud Functions & AngularFire1'
+      }
+    });
+
+    batch.update(firebaseCourseRef, {
+      titles: {
+        description: 'The Complete Typescript Course1',
+        longDescription: 'Complete Guide to Typescript From Scratch: Learn the language in-depth and use it to build a Node REST API.1'
+      }
+    });
+
+    const batch$ = of(batch.commit());
+    batch$.subscribe();
   }
 
 }
