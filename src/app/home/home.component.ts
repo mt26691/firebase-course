2f1bd77a9a28617b43828530d9234ec26c97a73f
mt2606 <nguyenmanhtung848@gmail.com>
2020-08-02 10:42:44 +0700

    add course servies

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { Course } from "../model/course";
-import { Observable } from "rxjs";
-import { map } from "rxjs/operators";
-import { AngularFirestore } from '@angular/fire/firestore';
-
+import {Component, OnInit} from '@angular/core';
+import {Course} from '../model/course';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+import {AngularFirestore} from '@angular/fire/firestore';
+import {CoursesService} from '../services/courses.service';
 
 
 @Component({
@@ -13,31 +13,24 @@ import { AngularFirestore } from '@angular/fire/firestore';
 })
 export class HomeComponent implements OnInit {
 
-
     courses$: Observable<Course[]>;
 
-    beginnerCourse$: Observable<Course[]>;
-    advancedCourser$: Observable<Course[]>;
+    beginnersCourses$: Observable<Course[]>;
+
+    advancedCourses$: Observable<Course[]>;
+
     constructor(private coursesService: CoursesService) {
 
     }
 
     ngOnInit() {
-        this.courses$ = this.db.collection('courses').snapshotChanges()
-            .pipe(map(snaps => {
-                return snaps.map(snap => {
-                    return <Course>{
-                        id: snap.payload.doc.id,
-                        ...(snap.payload.doc.data() as any)
-                    };
-                });
-            }));
+        this.courses$ = this.coursesService.loadAllCourses();
 
-        this.beginnerCourse$ = this.courses$.pipe(map(courses => courses.filter(course => 
+        this.beginnersCourses$ = this.courses$.pipe(map(courses => courses.filter(course => 
              course.categories.includes('BEGINNER')
         )));
 
-        this.advancedCourser$ = this.courses$.pipe(map(courses => courses.filter(course => 
+        this.advancedCourses$ = this.courses$.pipe(map(courses => courses.filter(course => 
             course.categories.includes('ADVANCED')
        )));
     }
--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -5,6 +5,7 @@ import { Course } from "../model/course";
 import { Injectable } from "@angular/core";
 import { AngularFirestore } from "@angular/fire/firestore";
 import { Observable } from "rxjs";
+import { map } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class CoursesService {
