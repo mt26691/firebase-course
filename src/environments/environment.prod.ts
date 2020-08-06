9a396be6304d4be317ef1aad209f94f8e816c149
mt26691 <tung.nguyen@scopicsoftware.com>
2020-08-06 10:25:31 +0700

    init firebase hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAyfENgWfFGgtZgIEtvFYosQ39ms-HE9_g',
+    authDomain: 'learning-firebase-c4c51.firebaseapp.com',
+    databaseURL: 'https://learning-firebase-c4c51.firebaseio.com',
+    projectId: 'learning-firebase-c4c51',
+    storageBucket: 'learning-firebase-c4c51.appspot.com',
+    messagingSenderId: '775045844459',
+    appId: '1:775045844459:web:502f8e28cfe16b9f241dae',
+    measurementId: 'G-C5262Y9M6K'
+  }
 };
