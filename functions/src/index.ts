51ee4a457e9616daad286552fb71a1f3ff026005
mt26691 <tung.nguyen@scopicsoftware.com>
2020-08-08 14:33:53 +0700

    init cloud functions

---
+++ functions/src/index.ts
@@ -0,0 +1,21 @@
+import * as functions from 'firebase-functions';
+import * as express from 'express';
+import { db } from './init';
+import * as cors from 'cors';
+
+const app = express();
+
+app.use(cors({ origin: true }));
+app.get('/courses', async (request, response) => {
+    const snaps = await db.collection('courses').get();
+
+    const courses: Array<any> = [];
+
+    snaps.forEach(snap => {
+        courses.push(snap.data());
+    });
+
+    response.status(200).json(courses);
+});
+
+export const getCourses = functions.https.onRequest(app);
