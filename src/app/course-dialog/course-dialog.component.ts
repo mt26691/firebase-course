fd4b9bf9355b321cce99463c9fad06286138c910
mt26691 <tung.nguyen@scopicsoftware.com>
2020-08-08 09:59:08 +0700

    get download url

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -3,6 +3,10 @@ import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
 import { Course } from "../model/course";
 import { FormBuilder, Validators, FormGroup } from "@angular/forms";
 import { CoursesService } from 'app/services/courses.service';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { Observable } from 'rxjs';
+import { last, concatMap } from 'rxjs/operators';
 
 
 @Component({
@@ -16,10 +20,14 @@ export class CourseDialogComponent implements OnInit {
     description: string;
     course: Course;
 
+    uploadPercent$: Observable<number>;
+
+    donwloadUrl$: Observable<string>;
     constructor(
         private fb: FormBuilder,
         private dialogRef: MatDialogRef<CourseDialogComponent>,
         private coursesService: CoursesService,
+        private storage: AngularFireStorage,
         @Inject(MAT_DIALOG_DATA) course: Course) {
 
         this.course = course;
@@ -46,6 +54,22 @@ export class CourseDialogComponent implements OnInit {
 
     close() {
         this.dialogRef.close();
+    }
+
+    uploadFile(event) {
+        const file: File = event.target.files[0];
+
+        const filePath = `courses/${this.course.id}/${file.name}`;
+
+        const task = this.storage.upload(filePath, file);
+        this.uploadPercent$ = task.percentageChanges();
+
+        this.donwloadUrl$ =  task.snapshotChanges()
+            .pipe(last(),
+                concatMap(() => this.storage.ref(filePath).getDownloadURL())
+            );
+
+        this.donwloadUrl$.subscribe(console.log);
     }
 
 }
