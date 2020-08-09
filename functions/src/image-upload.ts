fafeb4e002387a0094b978ed71ee4ca8b7a361ac
mt2606 <nguyenmanhtung848@gmail.com>
2020-08-09 11:48:08 +0700

    download file

--- functions/src/image-upload.ts
+++ functions/src/image-upload.ts
@@ -1,5 +1,10 @@
 import * as functions from 'firebase-functions';
 import * as path from 'path';
+import { Storage } from '@google-cloud/storage';
+import * as os from 'os';
+import * as mkdirp from 'mkdirp-promise';
+
+const gcs = new Storage();
 
 export const resizeThumbnail = functions.storage.object()
     .onFinalize(async (object, context) => {
@@ -7,11 +12,21 @@ export const resizeThumbnail = functions.storage.object()
         const contentType = object.contentType;
         const fileDir = path.dirname(fileFullPath);
         const fileName = path.basename(fileFullPath);
+        const tempLocalDir = path.join(os.tmpdir(), fileDir);
 
         console.log(`Thumbnail generation started`, fileFullPath, fileDir, fileName);
 
-        if (contentType?.startsWith('image/')) {
+        if (!contentType?.startsWith('image/')) {
+            console.log(`exit image processing`);
             return null;
         }
+
+        await mkdirp(tempLocalDir);
+        const bucket = gcs.bucket(object.bucket);
+        const orignalImageFile = bucket.file(fileFullPath);
+        const tempLocalFile = path.join(os.tmpdir(), fileFullPath);
+        await orignalImageFile.download({ destination: tempLocalFile });
+        console.log(`Downloading image to: `, tempLocalFile);
+
         return null;
     });
