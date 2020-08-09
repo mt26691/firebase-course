3d8b81547c37cb9802757894adfd110ae17f4239
mt2606 <nguyenmanhtung848@gmail.com>
2020-08-09 12:18:36 +0700

    upload thumbnail

--- functions/src/image-upload.ts
+++ functions/src/image-upload.ts
@@ -3,6 +3,7 @@ import * as path from 'path';
 import { Storage } from '@google-cloud/storage';
 import * as os from 'os';
 import * as mkdirp from 'mkdirp-promise';
+import { spawn } from 'child-process-promise';
 
 const gcs = new Storage();
 
@@ -16,7 +17,7 @@ export const resizeThumbnail = functions.storage.object()
 
         console.log(`Thumbnail generation started`, fileFullPath, fileDir, fileName);
 
-        if (!contentType?.startsWith('image/')) {
+        if (!contentType?.startsWith('image/') || fileName.startsWith('thumb_')) {
             console.log(`exit image processing`);
             return null;
         }
@@ -27,6 +28,27 @@ export const resizeThumbnail = functions.storage.object()
         const tempLocalFile = path.join(os.tmpdir(), fileFullPath);
         await orignalImageFile.download({ destination: tempLocalFile });
         console.log(`Downloading image to: `, tempLocalFile);
+
+        // Generate a thumbnail using image magik
+        const outputFilePath = path.join(fileDir, 'thumb_' + fileName);
+
+        const outputFile = path.join(os.tmpdir(), outputFilePath);
+
+        console.log(`generating a thumbnail to ${outputFile}`);
+
+        await spawn('convert', [tempLocalFile, '-thumbnail', '510x287 >', outputFile], {
+            capture: ['stdout', 'stderr']
+        });
+        // Upload the thumbnail to storage
+
+        const metadata = {
+            contentType: object.contentType,
+            cacheControl: `public,max-age=2592000, s-maxage=2592000`
+        };
+
+        console.log(`uploading the thumbnail to storage`, outputFile, outputFilePath);
+
+        await bucket.upload(outputFile, { destination: outputFilePath, metadata });
 
         return null;
     });
